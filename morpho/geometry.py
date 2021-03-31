730482ae24c2dfee478e8a530b8f63c59852154b
Tiago Vilela <tiagovla@gmail.com>
2021-03-30 23:06:18 -0300

    Refactored geometry.

--- morpho/geometry.py
+++ morpho/geometry.py
@@ -1,83 +1,32 @@
 """Implement photonic crystal geometries."""
 
-from typing import Callable, Optional, Tuple
+from abc import ABC
+from collections import namedtuple
+from typing import Callable, NamedTuple, Optional, Tuple
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Geometry:
-    """Geometry class.
+class GeometryBase(ABC):
+    """Base Geometry class."""
 
-    Parameters
-    ----------
-    t1 : Tuple[float, ...]
-        Direct t1 vector.
-    t2 : Optional[Tuple[float, ...]]
-        Direct t2 vector.
-    t3 : Optional[Tuple[float, ...]]
-        Direct t3 vector.
-    N1 : int
-        Number of divisions in the t1 vector direction.
-    N2 : int
-        Number of divisions in the t2 vector direction.
-    N3 : int
-        Number of divisions in the t3 vector direction.
-    eps_r : Optional[np.ndarray]
-        Permittivity matrix eps_r if directly supplied.
-    mu_r : Optional[np.ndarray]
-        Permeabillity matrix mu_r if directly supplied.
-    """
+    def __init__(self, eps_rf: Optional[Callable], mu_rf: Optional[Callable]):
+        self.eps_rf: Optional[Callable] = eps_rf or None
+        self.mu_rf: Optional[Callable] = mu_rf or None
+        self._eps_r: np.ndarray
+        self._mu_r: np.ndarray
 
-    def __init__(
-        self,
-        t1: Tuple[float, ...],
-        t2: Optional[Tuple[float, ...]] = None,
-        t3: Optional[Tuple[float, ...]] = None,
-        N1: int = 32,
-        N2: int = 1,
-        N3: int = 1,
-        eps_r: Optional[np.ndarray] = None,
-        mu_r: Optional[np.ndarray] = None,
-    ):
-        """Initialize the geometry obj."""
-        self.dim = len(t1)
-
-        self._t1 = np.pad(np.array(t1), (0, 3 - len(t1)), "constant")
-        self._t2 = (np.pad(np.array(t2), (0, 3 - len(t2)), "constant")
-                    if t2 else np.array([0, 1, 0]))
-        self._t3 = (np.pad(np.array(t3), (0, 3 - len(t1)), "constant")
-                    if t3 else np.array([0, 0, 1]))
-
-        self._T1 = (2 * np.pi * np.cross(self._t2, self._t3) /
-                    np.dot(self._t1, np.cross(self._t2, self._t3)))
-        self._T2 = (2 * np.pi * np.cross(self._t3, self._t1) /
-                    np.dot(self._t1, np.cross(self._t2, self._t3)))
-        self._T3 = (2 * np.pi * np.cross(self._t1, self._t2) /
-                    np.dot(self._t1, np.cross(self._t2, self._t3)))
-
-        self.eps_r = eps_r or np.ones((N1, N2, N3), dtype=complex)
-        self.mu_r = mu_r or np.ones((N1, N2, N3), dtype=complex)
-
-        P0, Q0, R0 = np.meshgrid(
-            np.linspace(-0.5, 0.5, N1),
-            np.linspace(-0.5, 0.5, N2) if N2 > 1 else 0,
-            np.linspace(-0.5, 0.5, N3) if N3 > 1 else 0,
-        )
-
-        self.x = P0 * self._t1[0] + Q0 * self._t2[0] + R0 * self._t3[0]
-        self.y = P0 * self._t1[1] + Q0 * self._t2[1] + R0 * self._t3[1]
-        self.z = P0 * self._t1[2] + Q0 * self._t2[2] + R0 * self._t3[2]
-
-        self.eps_rf: Optional[Callable] = None
-        self.mu_rf: Optional[Callable] = None
-
-    def setup(self):
-        """Run the epsr_f and mur_f if declared by the set decorators."""
+    @property
+    def eps_r(self) -> np.ndarray:
         if self.eps_rf:
             self.eps_rf()
+        return self._eps_r
+
+    @property
+    def mu_r(self) -> np.ndarray:
         if self.mu_rf:
             self.mu_rf()
+        return self._mu_r
 
     def set_eps_rf(self, func: Callable):
         """Set eps_r by a function decorator.
@@ -100,3 +49,179 @@ class Geometry:
         """
         self.mu_rf = func
         return func
+
+    def overwrite(self, func: Callable):
+        """Overwrite decorator.
+
+        Parameters
+        ----------
+        func : Callable
+            func
+        """
+        if func.__name__ in ['eps_rf', 'mu_rf']:
+            setattr(self, func.__name__, func)
+        else:
+            raise Exception("Can only overwrite eps_rf or mu_rf.")
+
+
+class Geometry1D(GeometryBase):
+    """Geometry class.
+
+    Parameters
+    ----------
+    a1 : Tuple[float, ...]
+        Direct a1 vector.
+    n1 : int
+        Number of divisions in the a1 vector direction.
+    eps_rf : Optional[Callable]
+        Permittivity matrix eps_r if directly supplied.
+    mu_rf : Optional[Callable]
+        Permeabillity matrix mu_r if directly supplied.
+    """
+
+    def __init__(self,
+                 a1: Tuple[float, float],
+                 n1: int = 64,
+                 eps_rf: Optional[Callable] = None,
+                 mu_rf: Optional[Callable] = None):
+        super().__init__(eps_rf, mu_rf)
+        self.a1: np.ndarray = np.array(a1)
+        self.n1: int = n1
+        self._eps_r: np.ndarray = np.ones((n1, ), dtype=complex)
+        self._mu_r: np.ndarray = np.ones((n1, ), dtype=complex)
+
+    @property
+    def X(self) -> NamedTuple:
+        X = namedtuple('CartesianVectors1D', ['x'])
+        P0 = np.linspace(-0.5, 0.5, self.n1)
+        x = P0 * self.a1[0]
+        return X(x)
+
+
+class Geometry2D(GeometryBase):
+    """Geometry class.
+
+    Parameters
+    ----------
+    a1 : Tuple[float, ...]
+        Direct a1 vector.
+    a2 : Optional[Tuple[float, ...]]
+        Direct a2 vector.
+    n1 : int
+        Number of divisions in the a1 vector direction.
+    n2 : int
+        Number of divisions in the a2 vector direction.
+    eps_rf : Optional[Callable]
+        Permittivity matrix eps_r if directly supplied.
+    mu_rf : Optional[Callable]
+        Permeabillity matrix mu_r if directly supplied.
+    """
+
+    def __init__(self,
+                 a1: Tuple[float, float],
+                 a2: Tuple[float, float],
+                 n1: int = 64,
+                 n2: int = 64,
+                 eps_rf: Optional[Callable] = None,
+                 mu_rf: Optional[Callable] = None):
+        super().__init__(eps_rf, mu_rf)
+        self.a1: np.ndarray = np.array(a1)
+        self.a2: np.ndarray = np.array(a2)
+        self.n1: int = n1
+        self.n2: int = n2
+        self._eps_r: np.ndarray = np.ones((n1, n2), dtype=complex)
+        self._mu_r: np.ndarray = np.ones((n1, n2), dtype=complex)
+
+    @property
+    def X(self) -> NamedTuple:
+        X = namedtuple('CartesianVectors2D', ['x', 'y'])
+
+        P0, Q0 = np.meshgrid(
+            np.linspace(-0.5, 0.5, self.n1),
+            np.linspace(-0.5, 0.5, self.n2),
+        )
+
+        x = P0 * self.a1[0] + Q0 * self.a2[0]
+        y = P0 * self.a1[1] + Q0 * self.a2[1]
+
+        return X(x, y)
+
+
+class Geometry3D(GeometryBase):
+    """Geometry class.
+
+    Parameters
+    ----------
+    a1 : Tuple[float, float, float]
+        Direct a1 vector.
+    a2 : Optional[Tuple[float, float, float]]
+        Direct a2 vector.
+    a3 : Optional[Tuple[float, float, float]]
+        Direct a3 vector.
+    n1 : int
+        Number of divisions in the a1 vector direction.
+    n2 : int
+        Number of divisions in the a2 vector direction.
+    n3 : int
+        Number of divisions in the a3 vector direction.
+    eps_rf : Optional[Callable]
+        Permittivity matrix function eps_rf if directly supplied.
+    mu_rf : Optional[Callable]
+        Permeabillity matrix function mu_rf if directly supplied.
+    """
+
+    def __init__(self,
+                 a1: Tuple[float, float, float],
+                 a2: Tuple[float, float, float],
+                 a3: Tuple[float, float, float],
+                 n1: int = 64,
+                 n2: int = 64,
+                 n3: int = 64,
+                 eps_rf: Optional[Callable] = None,
+                 mu_rf: Optional[Callable] = None):
+        super().__init__(eps_rf, mu_rf)
+        self.a1: np.ndarray = np.array(a1)
+        self.a2: np.ndarray = np.array(a2)
+        self.a3: np.ndarray = np.array(a3)
+        self.n1: int = n1
+        self.n2: int = n2
+        self.n3: int = n3
+        self._eps_r: np.ndarray = np.ones((n1, n2, n3), dtype=complex)
+        self._mu_r: np.ndarray = np.ones((n1, n2, n3), dtype=complex)
+
+    @property
+    def b1(self) -> np.ndarray:
+        return (2 * np.pi * np.cross(self.a2, self.a3) /
+                np.dot(self.a1, np.cross(self.a2, self.a3)))
+
+    @property
+    def b2(self) -> np.ndarray:
+        return (2 * np.pi * np.cross(self.a2, self.a3) /
+                np.dot(self.a1, np.cross(self.a2, self.a3)))
+
+    @property
+    def b3(self) -> np.ndarray:
+        return (2 * np.pi * np.cross(self.a2, self.a3) /
+                np.dot(self.a1, np.cross(self.a2, self.a3)))
+
+    @property
+    def X(self) -> NamedTuple:
+        X = namedtuple('CartesianVectors3D', ['x', 'y', 'z'])
+
+        P0, Q0, R0 = np.meshgrid(
+            np.linspace(-0.5, 0.5, self.n1),
+            np.linspace(-0.5, 0.5, self.n2),
+            np.linspace(-0.5, 0.5, self.n3),
+        )
+
+        x = P0 * self.a1[0] + Q0 * self.a2[0] + R0 * self.a3[0]
+        y = P0 * self.a1[1] + Q0 * self.a2[1] + R0 * self.a3[1]
+        z = P0 * self.a1[2] + Q0 * self.a2[2] + R0 * self.a3[2]
+
+        return X(x, y, z)
+
+
+def Geometry(*args, **kwargs):
+    """Geometry factory."""
+    dim_obj = {1: Geometry1D, 2: Geometry2D, 3: Geometry3D}
+    return dim_obj[len(args[0])](*args, **kwargs)
